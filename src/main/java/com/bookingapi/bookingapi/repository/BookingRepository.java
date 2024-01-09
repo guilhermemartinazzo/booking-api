e6708cf8d618e53cb6186cd64d58300400c3fed2
Guilherme <guilhermemartinazzo@hotmail.com>
2024-01-09 13:26:28 -0300

    fix: Fixing query (order of the parameters)

--- src/main/java/com/bookingapi/bookingapi/repository/BookingRepository.java
+++ src/main/java/com/bookingapi/bookingapi/repository/BookingRepository.java
@@ -15,11 +15,11 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
 	List<Booking> findByPropertyIdAndStatus(@Param("propertyId") Long propertyId,
 			@Param("status") BookingStatus status);
 
-	@Query(value = "SELECT b FROM Booking b where b.property.id = :idProperty and b.status IN ('ACTIVE','BLOCKED') and (b.startDate between :startDate and :endDate OR b.endDate between :startDate and :endDate)")
+	@Query(value = "SELECT b FROM Booking b where b.property.id = :idProperty and b.status IN ('ACTIVE','BLOCKED') and (:startDate between b.startDate and b.endDate OR :endDate between b.startDate and b.endDate)")
 	List<Booking> findBlocksFromPropertyBetweenDates(@Param("idProperty") Long idProperty,
 			@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 	
-	@Query(value = "SELECT b FROM Booking b where b.property.id = :idProperty and b.status IN :status and (b.startDate between :startDate and :endDate OR b.endDate between :startDate and :endDate)")
+	@Query(value = "SELECT b FROM Booking b where b.property.id = :idProperty and b.status IN :status and (:startDate between b.startDate and b.endDate OR :endDate between b.startDate and b.endDate)")
 	List<Booking> findBookingsFromPropertyBetweenDates(@Param("idProperty") Long idProperty,
 			@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate, @Param("status") List<BookingStatus> status);
 }
